b0e7bda2633d55efe0a87ade052b54331b20c37e
= <riccardo.parola@studenti.untin.it>
2022-04-24 15:32:38 +0200

    typo

--- real_time_inference.py
+++ real_time_inference.py
@@ -21,6 +21,7 @@ def lendmarks(image, detector, shape_predictor):
     dets = detector(image, 1)
     if len(dets) <= 0:
         print("no face landmark detected")
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         return image
     else:
         shape = shape_predictor(image, dets[0])
@@ -60,43 +61,43 @@ def lendmarks(image, detector, shape_predictor):
         quad = np.stack([c - x - y, c - x + y, c + x + y, c + x - y])
         qsize = np.hypot(*x) * 2
         
-        image = Image.fromarray(image)
-        image = image.convert('RGB')
+        pil_Image = Image.fromarray(image)
+        pil_Image = image.convert('RGB')
 
         # Shrink.
         shrink = int(np.floor(qsize / output_size * 0.5))
         if shrink > 1:
-            rsize = (int(np.rint(float(image.size[0]) / shrink)), int(np.rint(float(image.size[1]) / shrink)))
-            image = image.resize(rsize, PIL.Image.ANTIALIAS)
+            rsize = (int(np.rint(float(pil_Image.size[0]) / shrink)), int(np.rint(float(pil_Image.size[1]) / shrink)))
+            pil_Image = pil_Image.resize(rsize, PIL.Image.ANTIALIAS)
             quad /= shrink
             qsize /= shrink
 
         # Crop.
         border = max(int(np.rint(qsize * 0.1)), 3)
         crop = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
-        crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, image.size[0]), min(crop[3] + border, image.size[1]))
-        if crop[2] - crop[0] < image.size[0] or crop[3] - crop[1] < image.size[1]:
-            image = image.crop(crop)
+        crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, pil_Image.size[0]), min(crop[3] + border, pil_Image.size[1]))
+        if crop[2] - crop[0] < pil_Image.size[0] or crop[3] - crop[1] < pil_Image.size[1]:
+            pil_Image = pil_Image.crop(crop)
             quad -= crop[0:2]
 
         # Pad.
         pad = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
-        pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - image.size[0] + border, 0), max(pad[3] - image.size[1] + border, 0))
+        pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - pil_Image.size[0] + border, 0), max(pad[3] - pil_Image.size[1] + border, 0))
         if enable_padding and max(pad) > border - 4:
             pad = np.maximum(pad, int(np.rint(qsize * 0.3)))
-            image = np.pad(np.float32(image), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
-            h, w, _ = image.shape
+            pil_Image = np.pad(np.float32(pil_Image), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
+            h, w, _ = pil_Image.shape
             y, x, _ = np.ogrid[:h, :w, :1]
             mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w-1-x) / pad[2]), 1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h-1-y) / pad[3]))
             blur = qsize * 0.02
-            image += (scipy.ndimage.gaussian_filter(image, [blur, blur, 0]) - image) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
-            image += (np.median(image, axis=(0,1)) - image) * np.clip(mask, 0.0, 1.0)
+            pil_Image += (scipy.ndimage.gaussian_filter(pil_Image, [blur, blur, 0]) - pil_Image) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
+            pil_Image += (np.median(pil_Image, axis=(0,1)) - pil_Image) * np.clip(mask, 0.0, 1.0)
         # Transform.
-        image = image.transform((transform_size, transform_size), PIL.Image.QUAD, (quad + 0.5).flatten(), PIL.Image.BILINEAR)
+        pil_Image = pil_Image.transform((transform_size, transform_size), PIL.Image.QUAD, (quad + 0.5).flatten(), PIL.Image.BILINEAR)
         if output_size < transform_size:
-            image = image.resize((output_size, output_size), PIL.Image.ANTIALIAS)
+            pil_Image = pil_Image.resize((output_size, output_size), PIL.Image.ANTIALIAS)
 
-        return image
+        return pil_Image
 
 
 
